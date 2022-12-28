68cd032c207fa48435ef50b97395d7e5752bb9aa
eblyncho <eblyn52@naver.com>
2022-12-28 12:02:50 +0900

    Blueprint 적용

---
+++ pybo/views/main_views.py
@@ -0,0 +1,19 @@
+# main_views.py
+
+# from flask import Blueprint, render_template
+# from pybo.models import Question
+
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+@bp.route('/')
+def index() :
+    return redirect(url_for('question._list'))
+
+
+@bp.route('/hello')
+def hello_pybo() :
+    return 'Hello, pybooo'
+
