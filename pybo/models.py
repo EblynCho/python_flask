bbbb780e4d96a2beb32507ec6b527fef3e16173e
eblyncho <eblyn52@naver.com>
2022-12-28 12:03:21 +0900

    ORM 으로 테이블 선언

---
+++ pybo/models.py
@@ -0,0 +1,22 @@
+# models.py : ORM 으로 테이블 선언
+
+from pybo import db
+
+class Question(db.Model) :
+    id = db.Column(db.Integer, primary_key=True)  # Integer + primary_key -> auto increment 자동 적용됨
+    subject = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+
+class Answer(db.Model) :
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
+    question = db.relationship('Question', backref=db.backref('answer_set'))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+
+class User(db.Model) :
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
